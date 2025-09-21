2ac95be082e0123ad1c4ad667207f0bc0f91e47d
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-09-21 18:05:43 -0500

    chore: set default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import {LoginPage} from '../page/login/ui';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'login', component: LoginPage },
+];
