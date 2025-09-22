7e972999934e1020906adc13b89fc34cbe483f4a
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-09-22 18:27:32 -0500

    feat: implement service selection
    Adds a page allowing users to select a service type
    from a predefined list.
    
    Navigates to the equipment selection page after a
    service is selected.
    
    Includes basic styling for service selection and
    navigation buttons.

---
+++ src/page/select-service/ui/select-service/select-service.page.ts
@@ -0,0 +1,55 @@
+import {Component, computed, inject, OnInit, Signal, signal} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {ServiceTypeEnum} from '../../../../shared/model';
+import {Location, NgClass} from '@angular/common';
+import {Button} from 'primeng/button';
+
+interface Service {
+  id: ServiceTypeEnum;
+  name: string;
+  description: string;
+}
+
+@Component({
+  selector: 'app-select-service',
+  imports: [
+    NgClass,
+    Button
+  ],
+  templateUrl: './select-service.page.html',
+  standalone: true,
+  styleUrl: './select-service.page.css'
+})
+export class SelectServicePage implements OnInit{
+  readonly route = inject(ActivatedRoute);
+  readonly router = inject(Router);
+  private location = inject(Location);
+
+  projectId: string | null = null;
+  serviceSelected = signal<ServiceTypeEnum | null> ( null);
+  isServiceSelected = computed<boolean>(() => this.serviceSelected() !== null);
+  readonly services: Service[] = [
+    { id: ServiceTypeEnum.MAINTENANCE, name: 'Mantenimiento', description: 'Servicios de mantenimiento para asegurar el correcto funcionamiento de sus sistemas.' },
+    { id: ServiceTypeEnum.INSPECTION, name: 'Inspección', description: 'Servicios de inspección para evaluar el estado y desempeño de sus equipos.' },
+    { id: ServiceTypeEnum.RAISE_OBSERVATION, name: 'Levantamiento de Observaciones', description: 'Servicios para reportar y gestionar observaciones técnicas.' },
+  ]
+
+  ngOnInit(): void {
+    this.projectId = this.route.snapshot.paramMap.get('projectId');
+  }
+
+  protected selectService(serviceTypeEnum: ServiceTypeEnum) {
+    this.serviceSelected.set(serviceTypeEnum);
+  }
+
+  protected goBack() {
+    this.location.back();
+  }
+
+  protected navigateToEquipments() {
+    if (!this.projectId || !this.serviceSelected()) {
+      return;
+    }
+    // TODO: Navigate to equipment selection page
+  }
+}
