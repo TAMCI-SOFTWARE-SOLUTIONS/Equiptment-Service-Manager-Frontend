1bf0d254dbd4342abf39b8dbe79a185d0978c199
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-09-22 15:17:36 -0500

    chore: set route for clients

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'clients', title: 'Select Client', loadComponent: () => import('../page/select-client/ui').then(m => m.SelectClientPage) },
+];
