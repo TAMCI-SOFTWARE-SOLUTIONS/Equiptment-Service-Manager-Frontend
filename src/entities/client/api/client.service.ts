90a414eff26529fc8f8ede1afb215e3e145592cb
jhosepmyr <jhosepmyrgutierrezsoto@gmail.com>
2025-09-22 13:30:47 -0500

    feat: implement client entity and service
    Adds client entity model, DTO, mapper, and service to retrieve client data.
    
    This prepares the application to display a list of clients, fulfilling the requirements for the client selection page.

---
+++ src/entities/client/api/client.service.ts
@@ -0,0 +1,24 @@
+import {BaseService} from '../../../shared/api';
+import {Injectable} from '@angular/core';
+import {catchError, map, Observable, retry} from 'rxjs';
+import {ClientEntity} from '../model';
+import {ClientResponseDto} from './client-response.dto';
+import {ClientEntityFromResponseMapper} from './client-entity-from-response.mapper';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ClientService extends BaseService {
+  constructor() {
+    super();
+    this.resourceEndpoint = 'clients';
+  }
+
+  getAll(): Observable<ClientEntity[]> {
+    return this.http.get<ClientResponseDto[]>(`${this.resourcePath()}`, this.httpOptions).pipe(
+      map((clients: ClientResponseDto[]) => clients.map(client => ClientEntityFromResponseMapper.fromDtoToEntity(client))),
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+}
