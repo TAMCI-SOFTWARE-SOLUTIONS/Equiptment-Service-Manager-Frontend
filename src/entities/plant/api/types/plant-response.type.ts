84e6c3899dea0cea0159292f922b450acf1b0157
Jhosepmyr Orlando Gutierrez Soto <jhosepmyrgutierrezsoto@gmail.com>
2025-10-12 16:54:32 -0500

    feat(plant): add CreatePlantRequest type and mappers for plant entity

---
+++ src/entities/plant/api/types/plant-response.type.ts
@@ -0,0 +1,5 @@
+export interface PlantResponse {
+  id: string | null;
+  clientId: string | null;
+  name: string | null;
+}
---
+++ src/entities/plant/model/entities/plant.entity.ts
@@ -0,0 +1,5 @@
+export interface PlantEntity {
+  id: string;
+  clientId: string;
+  name: string;
+}
