3c4144bc750d698cdfb0cb32446176d06d7278a3
Jhosepmyr Orlando Gutierrez Soto <jhosepmyrgutierrezsoto@gmail.com>
2025-10-12 13:56:05 -0500

    feat(profile): add mappers and types for updating profile requests

---
+++ src/entities/profile/api/types/index.ts
@@ -0,0 +1,3 @@
+export type { CreateProfileRequest } from './create-profile-request.type';
+export type { UpdateProfileRequest } from './update-profile-request.type';
+export type { ProfileResponse } from './profile-response.type';
