fadc1340dc057f33d92ce623dc308b0697ea3df8
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-12 01:25:35 -0500

    feat(projects): add update and delete project methods in project service

--- src/entities/project/api/project.service.ts
+++ src/entities/project/api/project.service.ts
@@ -6,6 +6,8 @@ import {ProjectResponseDto} from './project-response.dto';
 import {ProjectEntityFromResponseMapper} from './project-entity-from-response.mapper';
 import {CreateProjectRequest} from './create-project-request.type';
 import {CreateProjectRequestFromEntityMapper} from './create-project-request-from-entity.mapper';
+import {UpdateProjectRequest} from './update-project-request.type';
+import {UpdateProjectRequestFromEntityMapper} from './update-project-request-from-entity.mapper';
 
 @Injectable({
   providedIn: 'root'
@@ -43,6 +45,22 @@ export class ProjectService extends BaseService {
   public getById(projectId: string): Observable<ProjectEntity> {
     return this.http.get<ProjectResponseDto>(`${this.resourcePath()}/${projectId}`, this.httpOptions).pipe(
       map((project: ProjectResponseDto) => ProjectEntityFromResponseMapper.fromDtoToEntity(project)),
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  update(projectId: string, entity: ProjectEntity): Observable<ProjectEntity> {
+    const request: UpdateProjectRequest = UpdateProjectRequestFromEntityMapper.fromEntityToDto(entity);
+    return this.http.put<ProjectResponseDto>(`${this.resourcePath()}/${projectId}`, request, this.httpOptions).pipe(
+      map((response: ProjectResponseDto) => ProjectEntityFromResponseMapper.fromDtoToEntity(response)),
+      retry(2),
+      catchError(this.handleError)
+    );
+  }
+
+  delete(projectId: string): Observable<void> {
+    return this.http.delete<void>(`${this.resourcePath()}/${projectId}`, this.httpOptions).pipe(
       retry(2),
       catchError(this.handleError)
     );
