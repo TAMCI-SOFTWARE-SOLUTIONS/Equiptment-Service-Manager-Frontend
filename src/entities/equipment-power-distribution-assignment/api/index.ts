bcfa2721386ce41e22cb8976e2e46e5bb8bbf36e
Jhosepmyr Orlando Gutierrez Soto <jhosepmyrgutierrezsoto@gmail.com>
2025-10-12 17:58:25 -0500

    feat(equipment-power-distribution): add types and service for equipment power distribution assignment

---
+++ src/entities/equipment-power-distribution-assignment/api/index.ts
@@ -0,0 +1,16 @@
+import {
+  CreateEquipmentPowerDistributionAssignmentRequestFromEntityMapper
+} from './mappers/create-equipment-power-distribution-assignment-request-from-entity.mapper';
+import {
+  EquipmentPowerDistributionAssignmentEntityFromResponseMapper
+} from './mappers/equipment-power-distribution-assignment-entity-from-response.mapper';
+import {EquipmentPowerDistributionAssignmentService} from './services/equipment-power-distribution-assignment.service';
+import {
+  CreateEquipmentPowerDistributionAssignmentRequest
+} from './types/create-equipment-power-distribution-assignment-request.type';
+import {
+  EquipmentPowerDistributionAssignmentResponse
+} from './types/equipment-power-distribution-assignment-response.type';
+
+export { CreateEquipmentPowerDistributionAssignmentRequestFromEntityMapper, EquipmentPowerDistributionAssignmentEntityFromResponseMapper, EquipmentPowerDistributionAssignmentService };
+export type { CreateEquipmentPowerDistributionAssignmentRequest, EquipmentPowerDistributionAssignmentResponse };
---
+++ src/entities/equipment-power-distribution-assignment/api/types/equipment-power-distribution-assignment-response.type.ts
@@ -0,0 +1,6 @@
+export interface EquipmentPowerDistributionAssignmentResponse {
+  id: string | null;
+  equipmentId: string | null;
+  powerDistributionPanelId: string | null;
+  circuitAssignments: any[] | null;
+}
