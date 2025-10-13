737a460151f17683efd29d52251ff561742d27aa
Jhosepmyr Orlando Gutierrez Soto <jhosepmyrgutierrezsoto@gmail.com>
2025-10-12 20:01:27 -0500

    feat(cabinet-type-form): create cabinet type form with validation, loading states, and custom styles

---
+++ page/cabinet-types/ui/cabinet-type-form/cabinet-type-form.page.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cabinet-type-form',
+  imports: [],
+  templateUrl: './cabinet-type-form.page.html',
+  styleUrl: './cabinet-type-form.page.css'
+})
+export class CabinetTypeFormPage {
+
+}
