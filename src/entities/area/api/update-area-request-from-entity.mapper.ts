8017f67b55bfc7e319b8b60ec7ed2b0c5138c043
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-13 21:08:46 -0500

    feat(mappers): add mappers for updating area, location, and plant requests

---
+++ src/entities/area/api/update-area-request-from-entity.mapper.ts
@@ -0,0 +1,15 @@
+import {UpdateAreaRequest} from './update-area-request.type';
+import {AreaEntity} from '../model';
+import {EquipmentTypeMapper} from './equipment-type.mapper';
+
+export class UpdateAreaRequestFromEntityMapper {
+  static fromEntityToDto(entity: AreaEntity): UpdateAreaRequest {
+    return {
+      name: entity.name,
+      code: entity.code,
+      allowedEquipmentTypes: entity.allowedEquipmentTypes.map(type =>
+        EquipmentTypeMapper.fromEnumToString(type)
+      ),
+    };
+  }
+}
