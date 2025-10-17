a322ca02f9c20a262cf84dc4273af398020f5706
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-17 18:01:08 -0500

    feat(profile): add identity document fields to ProfileEntity and mapper

--- src/entities/profile/api/mappers/profile-entity-from-response.mapper.ts
+++ src/entities/profile/api/mappers/profile-entity-from-response.mapper.ts
@@ -1,10 +1,14 @@
-import { ProfileResponse } from '../types/profile-response.type';
+import { ProfileResponse } from '../types';
 import { ProfileEntity } from '../../model';
 import { GenderMapper } from './gender.mapper';
+import {IdentityDocumentTypeMapper} from './identity-document-type.mapper';
 
 export class ProfileEntityFromResponseMapper {
   static fromDtoToEntity(dto: ProfileResponse): ProfileEntity {
     return {
+      identityDocumentNumber: dto.identityDocumentNumber ?? '',
+      identityDocumentType: IdentityDocumentTypeMapper.mapStringToIdentityDocumentType(dto.identityDocumentType?? ''),
+      shouldCreateUser: false,
       id: dto.id ?? '',
       userId: dto.userId ?? '',
       names: dto.names ?? '',
@@ -12,7 +16,7 @@ export class ProfileEntityFromResponseMapper {
       secondSurname: dto.secondSurname ?? '',
       photoFileId: dto.photoFileId ?? '',
       email: dto.email ?? '',
-      gender: GenderMapper.mapStringToGender(dto.gender ?? ''),
+      gender: GenderMapper.mapStringToGender(dto.gender ?? '')
     };
   }
 }
