716cbb8c6d7c19d5ceaf94aab974035d2a9e7323
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-17 17:44:48 -0500

    feat(service): add update cabinet and panel request types and mappers

---
+++ src/entities/cabinet/api/update-cabinet-request-from-entity.mapper.ts
@@ -0,0 +1,18 @@
+import { CabinetEntity } from '../model';
+import {UpdateCabinetRequest} from './update-cabinet-request.type';
+
+export class UpdateCabinetRequestFromEntityMapper {
+
+  static fromEntityToDto(entity: CabinetEntity): UpdateCabinetRequest {
+    return {
+      cabinetId: entity.id,
+      clientId: entity.clientId,
+      plantId: entity.plantId,
+      tag: entity.tag,
+      areaId: entity.areaId,
+      communicationProtocolId: entity.communicationProtocolId,
+      cabinetTypeId: entity.cabinetTypeId,
+      locationId: entity.locationId
+    };
+  }
+}
---
+++ src/entities/cabinet/api/update-cabinet-request.type.ts
@@ -0,0 +1,10 @@
+export interface UpdateCabinetRequest {
+  cabinetId: string | null;
+  tag: string | null;
+  clientId: string | null;
+  plantId: string | null;
+  areaId: string | null;
+  locationId: string | null;
+  communicationProtocolId: string | null;
+  cabinetTypeId: string | null;
+}
