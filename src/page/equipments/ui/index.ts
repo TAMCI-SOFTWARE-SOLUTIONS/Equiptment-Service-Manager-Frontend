62e1d5c0d9f00998cf7638858a59662ae36b141b
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-10-17 09:46:16 -0500

    feat(index): remove EquipmentsListPage export from index file

--- src/page/equipments/ui/index.ts
+++ src/page/equipments/ui/index.ts
@@ -1,5 +1,4 @@
-import { EquipmentsListPage } from './equipments-list/equipments-list.page';
 import { EquipmentDetailPage } from './equipment-detail/equipment-detail.page';
 import { EquipmentsPage } from './equipments';
 
-export { EquipmentsListPage, EquipmentDetailPage, EquipmentsPage };
+export { EquipmentDetailPage, EquipmentsPage };
