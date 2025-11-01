ccca8778606927318da3610aba946b605c22bc97
Gutierrez Soto Jhosepmyr Orlando <jhosepmyrgutierrezsoto@gmail.com>
2025-11-01 14:34:39 -0500

    refactor(evidence-types): add EvidenceFile interface and EvidenceType type definitions

---
+++ src/page/services/model/types/evidence-type.type.ts
@@ -0,0 +1,10 @@
+export type EvidenceType =
+  | 'videoStart'
+  | 'videoEnd'
+  | 'startPhoto'
+  | 'midPhoto'
+  | 'endPhoto'
+  | 'report';
+
+export type PhotoType = 'startPhoto' | 'midPhoto' | 'endPhoto';
+export type VideoType = 'videoStart' | 'videoEnd';
